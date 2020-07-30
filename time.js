645a1de1001a2685360923c548b9c9472051add9
Stephen <stephen@tca0.nl>
2020-07-31 01:04:58 +0200

    added timers

--- time.js
+++ time.js
@@ -1,3 +1,10 @@
 function timer() {
-    document.getElementById('timer').innerHTML(+Date.now())
+    setInterval(() => {
+        document.getElementById('timer').innerHTML = +new Date().UTC()
+    }, 1)
+
+    setInterval(() => {
+        document.getElementById('timer2').innerHTML = moment().format('MMMM Do YYYY, hh:mm:ss');
+    }, 1000)
+
 }
