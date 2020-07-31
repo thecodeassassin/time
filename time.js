d7dace4a853882520d8e4e3aabbca729c1672cd2
Stephen <stephen@tca0.nl>
2020-07-31 02:03:36 +0200

    better now

--- time.js
+++ time.js
@@ -1,10 +1,11 @@
 function timer() {
+    document.getElementById('date').innerHTML = moment().format('MMMM Do YYYY');
     setInterval(() => {
         document.getElementById('timer').innerHTML = +new Date()
     }, 1)
 
     setInterval(() => {
-        document.getElementById('timer2').innerHTML = moment().format('MMMM Do YYYY, hh:mm:ss');
+        document.getElementById('timer2').innerHTML = moment().format('hh:mm:ss');
     }, 1000)
 
 }
